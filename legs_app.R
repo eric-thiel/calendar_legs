b41e67eea7c5c12c9af98baa240bc518071d5d16
Eric Thiel <eric@gatherwholesale.com>
2020-09-21 16:04:50 -0600

    commit

---
+++ legs_app.R
@@ -0,0 +1,76 @@
+### function to put stats online
+
+### add passing attempts and rushing attempts????????????????
+### big brain SZN
+
+
+library("shiny")   
+library(DT)
+library(googlesheets4)
+library(shinycssloaders)
+library(dplyr)
+library(rdrop2)
+library(readr)
+library(vroom)
+library(tidyverse)
+library(shinythemes)
+
+header.true <- function(df) {
+  names(df) <- as.character(unlist(df[1,]))
+  df[-1,]
+}
+
+
+ui = fluidPage( theme = shinytheme("darkly"),
+                pageWithSidebar(
+                  headerPanel("Should you Shave your Legs?"),
+                  
+                  actionButton("button",label = "Push Me Once Per Day"),
+                  
+                  mainPanel(
+                    htmlOutput("mytable"),
+                    tags$style("#mytable {font-size:80px;
+               position:relative;
+               color: lightsalmon;
+               }"),
+                    
+                  )
+                  
+                ))
+
+
+server = shinyServer(
+  function(input,output,session){
+    
+    cap = eventReactive(input$button, {
+      
+      number = runif(1,0,1)
+      ifelse(number > 0.3, "SHAVE YOUR LEGS","DON'T SHAVE YOUR LEGS")
+    }) 
+    
+    output$mytable = renderText({   
+      
+      
+      
+      
+      cap()
+    })
+  })
+
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
